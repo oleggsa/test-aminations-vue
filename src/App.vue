14df7d8fd34caf0ae4ec8c95f87d8b58a018ad82
Oleh Didyk <78662308+oleggsa@users.noreply.github.com>
2022-12-12 11:03:20 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
-  <router-view v-slot="{ Component }">
-    <transition name="slide">
-      <component :is="Component" />
-    </transition>
-  </router-view>
+ <RouterView />
 </template>
 
 <script>
@@ -11,24 +7,5 @@
 </script>
 
 <style lang="scss" scoped>
-.slide-enter-active,
-.slide-leave-active {
-  transition: all 0.75s ease-out;
-}
-.slide-enter-to {
-  position: absolute;
-  right: 0;
-}
-.slide-enter-from {
-  position: absolute;
-  right: -100%;
-}
-.slide-leave-to {
-  position: absolute;
-  left: -100%;
-}
-.slide-leave-from {
-  position: absolute;
-  left: 0;
-}
+
 </style>
